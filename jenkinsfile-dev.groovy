52581fe39559d6c3c329c71c130fef8a67c14cd7
Sungmin Kim <83997823+keeptrynjoy@users.noreply.github.com>
2023-12-17 12:56:55 +0900

    Update jenkinsfile-dev.groovy

--- jenkinsfile-dev.groovy
+++ jenkinsfile-dev.groovy
@@ -13,4 +13,29 @@ node {
     stage('Build') {
         sh "${gradleHome}/bin/gradle clean build"
     }
+
+     post {
+        success {
+            slackSend (
+                channel: '#build-log', 
+                color: '#00FF00', 
+                message: """
+SUCCESS 
+Job : ${env.JOB_NAME} - [#${env.BUILD_NUMBER}]
+<${env.BUILD_URL}|OPEN>
+"""
+            )
+        }
+        failure {
+            slackSend (
+                channel: '#build-log', 
+                color: '#FF0000', 
+                message: """
+FAIL 
+- Job : ${env.JOB_NAME} - [#${env.BUILD_NUMBER}]
+<${env.BUILD_URL}|OPEN>
+} 
+                """
+            )
+        }
 }
