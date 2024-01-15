3ba6169975443567379885c9c434f0e28e595242
keeptrynjoy <sungmin4218@gmail.com>
2024-01-15 21:38:48 +0900

    🛠️ 패키지 기본 세팅

---
+++ src/main/kotlin/com/uahannam/sellerservice/SellerServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.uahannam.sellerservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SellerServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SellerServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/uahannam/sellerservice/SellerServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.uahannam.sellerservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SellerServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
