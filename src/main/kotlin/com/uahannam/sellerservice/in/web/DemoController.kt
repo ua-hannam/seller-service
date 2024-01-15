b7191f7e19cd742d4536ec026dee3a7181a0d6c7
keeptrynjoy <sungmin4218@gmail.com>
2024-01-15 22:35:06 +0900

    🚀 Sonar Cloud test

---
+++ src/main/kotlin/com/uahannam/sellerservice/in/web/DemoController.kt
@@ -0,0 +1,17 @@
+package com.uahannam.sellerservice.`in`.web
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/seller")
+class DemoController {
+
+    @GetMapping("/test")
+    fun testSeller() : ResponseEntity<String> {
+        return ResponseEntity.ok("success");
+    }
+}
