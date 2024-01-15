b475f13bf139327abcb1599a26ea4ca741817ab4
keeptrynjoy <sungmin4218@gmail.com>
2024-01-15 22:24:42 +0900

    🚀 Sonar Cloud 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.12"
 	id("io.spring.dependency-management") version "1.1.3"
+	id("org.sonarqube") version "4.4.1.3373"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.4.32"
@@ -27,6 +28,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "ua-hannam_seller-service")
+		property("sonar.organization", "msa2024")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 tasks.withType<KotlinCompile> {
