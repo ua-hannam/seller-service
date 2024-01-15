0e1d7c94c44fd52750f8c66b91ccc60558a38996
keeptrynjoy <sungmin4218@gmail.com>
2024-01-15 22:37:09 +0900

    🚀 Sonar Cloud test

--- src/main/kotlin/com/uahannam/sellerservice/in/web/DemoController.kt
+++ src/main/kotlin/com/uahannam/sellerservice/in/web/DemoController.kt
@@ -1,6 +1,5 @@
 package com.uahannam.sellerservice.`in`.web
 
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
